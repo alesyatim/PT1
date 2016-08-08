0dadb8dc940d9fe688c1f1f2bac430fbabec4f43
Pyautomation <Pyautomation@dd.by>
2016-08-08 21:18:01 +0300

    2

--- Class 3.py
+++ Class 3.py
@@ -9,4 +9,81 @@ for key in keys:
     print('{}={}'.format(key,d[key]))
 
 
+def printer(x, y):
+    print(x)
 
+printer(y=1, x ='b')
+########################
+def whisper(word):
+    print(word.lower())
+
+def shout(word):
+    print(word.upper() + '!!!!')
+
+def say(volume, word='I am talking'):
+    volume(word)
+
+time =1
+method = shout
+for hour in range(10):
+    if  hour > 5:
+        method = whisper
+    say(method)
+
+
+if time > 9:
+    method = whisper
+
+
+
+say(shout)
+say(whisper, word='HEY!!!!')
+
+
+
+#####################################
+L = ['one', 'two', 'three']
+D = {'1': 2, '3': 4}
+def catcher(*args, **kwargs):
+    print(args)
+    print(type(args[0]))
+    print(kwargs)
+
+catcher(L, d=D)
+catcher(*L, **D)
+#catcher(1,2,3, one=1, two='two')
+########################
+def hey():
+    print('Hey!')
+
+def catcher(*args):
+    for _ in args:
+        if callable(_):
+            print('Cotta function!')
+            _()
+        else:
+            print(_)
+
+
+catcher(1,2,3, hey)
+#####################
+
+#def hey(x):
+#    x(x)
+
+#hey(hey)
+
+###################
+def f(a, L=[]):
+    L.append(a)
+    return L
+print f(1)
+print f(2)
+print f(3)
+
+def f(a, b, L={}):
+    L[a] = b
+    return L
+print f('1', 1)
+print f('2', 2)
+print f('3', 3)
