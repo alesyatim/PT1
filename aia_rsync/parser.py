e8a8544d2c01bf66b74b80c8b801b7798338aa2a
alesya <alesya_tim@tut.by>
2016-08-19 14:29:57 +0300

    keys

--- aia_rsync/parser.py
+++ aia_rsync/parser.py
@@ -14,15 +14,14 @@ def parse_data(in_str):
 def parse_keys(in_str):
     pattern = re.compile('(-[a-z ]+) ')
     res = pattern.findall(in_str)
-    print(res)
     add_keys(res)
     return err.errors['Ok']
 
 def parse_args(in_str):
     pass
 
-def add_keys(*in_keys):
-    pass
+def add_keys(in_keys):
+    keys[:] = in_keys[:]
 
 def add_arguments(**kwargs):
     pass
@@ -45,4 +44,5 @@ if __name__ == '__main__':
 
     test_str = '-v -as -progress -e ssh -i -P /dir'
     parse_keys(test_str)
+    print(keys)
 
