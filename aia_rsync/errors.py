3360d3f0f36eaf415bf54f41d5e0b95342283015
alesya <alesya_tim@tut.by>
2016-08-22 16:04:24 +0300

    we will not write and use own errors

--- aia_rsync/errors.py
+++ aia_rsync/errors.py
@@ -1,17 +1,5 @@
-'''
-    module: Errors handler
-'''
-
-# Errors:
-# validations errors start from 1: 1xy
-# parse errors start from 2: 2xy
-# connection errors start from 3: 3xy
-# ...
 errors = {'Ok':0,
           'PathNotFound':101, 'NotFile':102, 'NotDir':103, 'NotPath':104,
           'ParseErr':201, # parse errors 2xy
          }
 
-#
-class ErrorHandler(Exception):
-    pass
