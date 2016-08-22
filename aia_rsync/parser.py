564d3a45363ccea75926c06d1e7a388ea51897b7
alesya <alesya_tim@tut.by>
2016-08-22 15:53:32 +0300

    check keys

--- aia_rsync/parser.py
+++ aia_rsync/parser.py
@@ -7,15 +7,29 @@ arguments = {
     'password':''
 }
 keys = []
+availible_single_keys = ['P', 'a', 'v', 'S', 'z', 'q', 'i']
+availible_complicated_keys = ['progress', 'e ssh']
 
 def parse_data(in_str):
      pass
 
 def parse_keys(in_str):
-    pattern = re.compile('(-[a-z ]+) ')
+    pattern = re.compile('-([a-z ]+) ')
     res = pattern.findall(in_str)
     add_keys(res)
     return err.errors['Ok']
+
+# def split_keys():
+#     tmp = [list(key) for key in keys if not check_complicated_keys(key)]
+#     print(tmp)
+
+def check_single_keys(key):
+    if key in availible_single_keys:
+        return True
+
+def check_complicated_keys(key):
+    if key in availible_complicated_keys:
+        return True
 
 def parse_args(in_str):
     pattern = re.compile('pass=([a-zA-Z0-9]+)')
@@ -66,4 +80,6 @@ if __name__ == '__main__':
     parse_keys(test_str)
     parse_args(test_str)
     print(keys, arguments)
+    split_keys()
+    #print(keys)
 
